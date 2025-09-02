c83c36142679856babf8db09acc5cd9ea6e47bde
waalidait <snwalid@gmail.com>
2025-09-02 16:24:28 +0200

    ex2

--- chap4ex2.c
+++ chap4ex2.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<math.h>
+
 int main(){
 float v,r;
 const float Pi=3.14;
-r=10;
-v=4.0/3.0*Pi*pow(r,3);
+printf("enter r: ");scanf("%f",&r);
+v=4.0/3.0*Pi*(r*r*r);
 printf("le volume du sphere = %f", v);
     return 0;
 }
